8121589dcedbd03a3efeadf34cd1bdf06e6475e7
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-07-29 23:06:31 -0500

    Rewrite (WIP)

--- coin.tcl
+++ coin.tcl
@@ -1,149 +1,165 @@
-#!/usr/bin/wish
+#!/usr/bin/tclsh
 package require Tk
 
-### Root
-wm title . "Coin Counter"
-wm geometry . "500x175"
-wm minsize . 500 175
-wm resizable . 1 0
-grid columnconfigure . 0 -weight 1
-grid rowconfigure . "0 2" -weight 1
-
-### Coin frame
-ttk::frame .coin_frame -padding 10
-grid .coin_frame -column 0 -row 0 -sticky nswe
-
-set cur_coin ""
-set cur_count 0
-proc coin_list {} {
-	lsort [array names ::collection]
+##### Backend
+array set collection ""
+proc array_out {array filename} {
+	upvar $array arr
+	set file [open $filename w]
+	puts $file [array get arr]
+	close $file
 }
-proc update_selector {} {
-	.coin_frame.selector configure -values [coin_list]
+proc array_in {array filename} {
+	upvar $array arr
+	set file [open $filename r]
+	array set arr [read $file]
+	close $file
 }
-proc update_count {} {
-	set ::cur_count $::collection($::cur_coin)
-}
-proc set_count {} {
-	set ::cur_coin [string trim $::cur_coin " "]
-	if {![string compare $::cur_coin ""]} return
-	set ::collection($::cur_coin) $::cur_count
-	update_selector ;# in case the value didn't exist before
-}
-
-ttk::combobox .coin_frame.selector -textvariable cur_coin -values ""
-bind .coin_frame.selector <<ComboboxSelected>> update_count
-bind .coin_frame.selector <Return>  set_count
-bind .coin_frame.selector <KP_Enter>  set_count
-ttk::spinbox .coin_frame.count -textvariable cur_count -command set_count \
-	-from 0 -to 1e6 -increment 1 -wrap 0 -justify left
-bind .coin_frame.count <Return> set_count
-bind .coin_frame.count <KP_Enter> set_count
-grid .coin_frame.selector -column 0 -row 0 -rowspan 2 -sticky we -padx 5
-grid .coin_frame.count -column 1 -row 0 -sticky we -padx 5 -pady "0 5"
-
-ttk::button .coin_frame.remover -text "Delete" \
-	-command {catch {unset ::collection($::cur_coin)}; update_selector}
-grid .coin_frame.remover -column 1 -row 1 -sticky nswe -padx 5
-
-grid columnconfigure .coin_frame 0 -weight 5
-grid columnconfigure .coin_frame 1 -weight 1
-grid rowconfigure .coin_frame "0 1" -weight 1
-
-### Separator
-grid [ttk::separator .sep -orient horizontal] -column 0 -row 1 -sticky we
-
-### File frame
-ttk::frame .file_frame -padding 10
-grid .file_frame -column 0 -row 2 -sticky nswe
-
-proc save_collection {filename} {
-	if [catch {set fh [open $filename w]}] {
-		tk_messageBox -type ok -icon warning -message "File not written"
-		return
+proc list_items {{search ""}} {
+	set names [lsort [array names ::collection]]
+	if {$search eq ""} {
+		return $names
 	}
-	puts $fh [array get ::collection]
-	close $fh
-}
-proc load_collection {filename} {
-	if [catch {set fh [open $filename r]}] {
-		tk_messageBox -type ok -icon warning -message "File not read"
-		return
-	}
-	catch {unset ::collection}
-	array set ::collection [gets $fh]
-	set ::cur_coin ""
-	set ::cur_count 0
-	close $fh
-}
-
-ttk::button .file_frame.saveb -text "Save File" -command {save_collection [tk_getSaveFile]}
-ttk::button .file_frame.loadb -text "Load File" -command {load_collection [tk_getOpenFile]; update_selector}
-grid .file_frame.saveb -column 0 -row 0 -sticky nswe -padx "0 5"
-grid .file_frame.loadb -column 1 -row 0 -sticky nswe -padx "5 0"
-
-grid rowconfigure .file_frame 0 -weight 1
-grid columnconfigure .file_frame "0 1" -weight 1
-
-if [expr $argc>0] {load_collection [lindex $argv 0]; update_selector}
-
-### Summary window
-set search ""
-proc show_summary {} {
-	if {![catch {toplevel .s}]} {
-		wm title .s "Summary"
-		wm resizable .s 1 0
-		wm minsize .s 200 -1
-	
-		frame .s.search
-		grid .s.search -column 0 -row 0 -sticky we
-		ttk::entry .s.search.bar -textvariable search
-		bind .s.search.bar <Return> update_summary_list
-		bind .s.search.bar <KP_Enter> update_summary_list
-		ttk::button .s.search.go -text "Search" -command update_summary_list
-		grid .s.search.bar -column 0 -row 0 -sticky we
-		grid .s.search.go -column 1 -row 0 -sticky nswe
-		grid columnconfigure .s.search 0 -weight 1
-	
-		frame .s.ct -background "#FFFFFF"
-		grid .s.ct -column 0 -row 1 -sticky nswe
-		ttk::label .s.ct.name -text "Name" -background "#FFFFFF"
-		ttk::label .s.ct.count -text "Count" -justify right -background "#FFFFFF"
-		grid .s.ct.name -column 0 -row 0 -sticky w
-		grid .s.ct.count -column 1 -row 0 -sticky e
-		grid columnconfigure .s.ct "0 1" -weight 1
-		grid columnconfigure .s 0 -weight 1
-	}
-	update_summary_list
-}
-proc update_summary_list {} {
-	destroy .s.dat
-	grid [frame .s.dat] -column 0 -row 2 -sticky nswe
-	grid columnconfigure .s.dat "0 1" -weight 1
-	set r 0
-	set s 0
-	foreach coin [coin_list] {
-		if {[string match -nocase *$::search* $coin]} {
-			add_summary_data $coin [incr r]
-			incr s $::collection($coin)
+	set results ""
+	foreach name $names {
+		if [string match -nocase *$search* $name] {
+			lappend results $name
 		}
 	}
-	.s.ct.name configure -text "Name ($r)"
-	.s.ct.count configure -text "Count ($s)"
+	return $results
 }
-proc add_summary_data {coin row} {
-	set color [expr ($row%2)?"#F0F0F0":"#FFFFFF"]
-	set update_cmd "set ::cur_coin {$coin}; update_count"
-	frame .s.dat.r$row -background $color
-	bind .s.dat.r$row <1> $update_cmd
-	ttk::label .s.dat.r$row.c -text $coin -background $color
-	bind .s.dat.r$row.c <1> $update_cmd
-	ttk::label .s.dat.r$row.cc -text $::collection($coin) -background $color -justify right
-	bind .s.dat.r$row.cc <1> $update_cmd
-	grid .s.dat.r$row -column 0 -row $row -columnspan 2 -sticky nswe
-	grid .s.dat.r$row.c -column 0 -row 0 -sticky w
-	grid .s.dat.r$row.cc -column 1 -row 0 -sticky e
-	grid columnconfigure .s.dat.r$row "0 1" -weight 1
+proc remove_item {name} {
+	catch {unset ::collection($name)}
 }
-ttk::button .sum -text "Summary" -command {set search ""; show_summary}
-grid .sum -column 0 -row 99 -columnspan 2 -pady "0 10"
+proc add_item {name {count 0}} {
+	if {$name ne ""} {
+		set ::collection($name) $count
+	}
+}
+proc edit_item {name new_name count} {
+	remove_item $name
+	add_item $new_name $count
+}
+
+##### Reader
+ttk::frame .reader
+grid .reader -column 0 -row 0 -sticky nswe
+grid columnconfigure . 0 -weight 1
+grid rowconfigure . 0 -weight 1
+
+proc update_selector {} {
+	.reader.selector configure -values [list_items [.reader.selector get]]
+}
+proc reset_selector {} {
+	set ::selected ""
+	.reader.count configure -text 0
+	update_selector
+}
+proc update_count {} {
+	.reader.count configure -text $::collection([.reader.selector get])
+}
+ttk::combobox .reader.selector -values "" -textvariable selected -postcommand update_selector
+set selected "Search"
+bind .reader.selector <<ComboboxSelected>> update_count
+ttk::label .reader.count -text "0"
+grid .reader.selector -column 0 -row 0 -sticky we
+grid .reader.count -column 1 -row 0
+grid columnconfigure .reader 0 -weight 5
+grid columnconfigure .reader 1 -weight 1
+grid rowconfigure .reader 0 -weight 1
+
+##### Controls
+ttk::frame .controls
+grid .controls -column 0 -row 1
+grid rowconfigure . 1 -weight 1
+
+ttk::button .controls.add -text "Add Entry" -command add_menu
+ttk::button .controls.summ -text "Summary" -command summary_menu
+ttk::button .controls.edit -text "Edit Entry" -command edit_menu
+grid .controls.add -column 0 -row 0 -sticky nswe
+grid .controls.summ -column 1 -row 0 -sticky nswe
+grid .controls.edit -column 2 -row 0 -sticky nswe
+grid columnconfigure .controls "0 1 2" -weight 1
+grid rowconfigure .controls 0 -weight 1
+
+##### Menus
+proc add_menu {} {
+	if [catch {toplevel .add}] return
+	grab set .add
+	wm title .add "Add Item"
+#	wm resizable .add 1 0
+	ttk::frame .add.menu
+	grid .add.menu -column 0 -row 0 -sticky nswe
+	grid columnconfigure .add 0 -weight 1
+	grid rowconfigure .add 0 -weight 1
+
+	set add_cmd {add_item $::addendum 0; reset_selector; destroy .add}
+	ttk::entry .add.menu.name -textvariable ::addendum
+	set ::addendum "New item"
+	.add.menu.name selection range 0 8
+	bind .add.menu.name <Return> $add_cmd
+	focus .add.menu.name
+	ttk::button .add.menu.go -text "Add" -command $add_cmd
+	grid .add.menu.name -column 0 -row 0 -sticky we
+	grid .add.menu.go -column 0 -row 1
+	grid columnconfigure .add.menu 0 -weight 1
+	grid rowconfigure .add.menu 0 -weight 1
+}
+proc summary_menu {} {
+}
+proc add_summary_item {item row} {
+	set color [expr $row%2?"#FFFFFF":"#F0F0F0"]
+	set update_cmd ".reader.selector configure -text $item; update_count"
+	ttk::frame .summ.items.r$row -background $color
+	bind .summ.items.r$row <1> update_cmd
+	ttk::label .summ.items.r$row.name -text $item -background $color
+	bind .summ.items.r$row.name <1> update_cmd
+	ttk::separator .summ.items.r$row.sep -orient vertical
+	bind .summ.items.r$row.sep <1> update_cmd
+	ttk::label .summ.items r$row.count -text $::collection($item) -background $color
+	bind .summ.items.r$row.count <1> update_cmd
+	grid .summ.items.r$row -column 0 -row $row -sticky we
+	grid .summ.items.r$row.name -column 0 -row 0 -sticky w
+	grid .summ.items.r$row.sep -column 1 -row 0 -sticky ns
+	grid .summ.items.r$row.count -column 2 -row 0 -sticky e
+	grid columnconfigure .summ.items.r$row "0 2" -weight 1
+}
+proc update_summary {} {
+	set names [list_items $::filter]
+	set sum 0
+	set row 1
+	foreach name $names {
+		incr sum $::collection($name)
+		add_summary_item $name $row
+	}
+}
+proc edit_menu {} {
+	set item [.reader.selector get]
+	if {![info exists ::collection($item)]} return
+	if [catch {toplevel .edit}] return
+	grab set .edit
+	wm title .edit "Editing $item"
+#	wm resizable .edit 1 0
+	ttk::frame .edit.menu
+	grid .edit.menu -column 0 -row 0 -sticky nswe
+	grid columnconfigure .edit 0 -weight 1
+	grid rowconfigure .edit 0 -weight 1
+
+	set edit_cmd "edit_item $item \$editname \$editcount; reset_selector; destroy .edit"
+	ttk::entry .edit.menu.name -textvariable ::editname
+	bind .edit.menu.name <Return> $edit_cmd
+	set ::editname $item
+	focus .edit.menu.name
+	.edit.menu.name selection range 0 [string length $item]
+	ttk::spinbox .edit.menu.count -from 0 -to 1000000 -increment 1 -wrap 0 -textvariable ::editcount
+	set ::editcount $::collection($item) 
+	bind .edit.menu.count <Return> $edit_cmd
+	ttk::button .edit.menu.submit -text "Submit" -command $edit_cmd
+	ttk::button .edit.menu.delete -text "Delete" -command "remove_item $item; reset_selector; destroy .edit"
+	grid .edit.menu.name -column 0 -row 0 -sticky we
+	grid .edit.menu.count -column 1 -row 0 -sticky we
+	grid .edit.menu.delete -column 0 -row 1
+	grid .edit.menu.submit -column 1 -row 1
+	grid columnconfigure .edit.menu 0 -weight 1
+}
+array_in collection sampledata
